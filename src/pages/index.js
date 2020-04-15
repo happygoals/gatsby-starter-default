f4def08d8f4030521fa806f5d5c19392da293767
Haemin Ryu <13105504+happygoals@users.noreply.github.com>
2020-04-15 11:50:01 -0400

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,10 +13,9 @@ const IndexPage = () => (
     <p className="intro1">Hello there! I’m</p>
     <h2 className="introname">Haemin Ryu.</h2>
     
-    <h3>I build changes so that <br /> humans' life better.</h3>
-    
+    <h3>I build seeing-beyond changes so that <br /> humans experience better.</h3>
     <p className="para">I'm a software engineer based in Fort Wayne, IN, USA <br />
-    Also know as a problem solver, design thinker, servant leader, creative maker.</p>
+    Also know as a problem solver, design thinker, servant leader, experience giver.</p>
     <div className="btnrow">
     <button className="button1"><a className="btnlink" href="mailto:developer.haemin.ryu@gmail.com">Get In Touch</a></button>
     <button className="button2" ><a className="btnlink" href="https://drive.google.com/open?id=1r6xtzN4Z8aZejxT4VhP-n3H7n0ReIn9R">&nbsp;Resume&nbsp;</a></button>
