5003c67a87bd69edabf5ebedd4cb2dfefbf9ae0d
Haemin Ryu <13105504+happygoals@users.noreply.github.com>
2021-01-13 16:42:34 -0500

    Update header.js

--- src/components/header.js
+++ src/components/header.js
@@ -6,10 +6,10 @@ export default function header() {
     <div>
       <div class="topnav">
        // <AniLink cover to="projects" bg="rgb(235, 210, 97)">
-          <a>Projects -></a>
+          //<a>Projects -></a>
           {/* <a>HireMe</a> */}
         </AniLink>
-        //<a href="http://happygoals.github.io/haemin-ryu-devlog">Blogs</a>
+        //<a href="#">Blogs</a>
       </div>
     </div>
   )
